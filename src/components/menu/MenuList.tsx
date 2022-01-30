91bcc2f882303a2dce3d76af1cdfc9fa5ff2fb39
Ameer <ameer.taghavi@gmail.com>
2022-01-30 19:49:03 +0330

    test

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -83,7 +83,7 @@ const MenuListComponent = () => {
 
     return {
       left: _leftPosition,
-      height: menuHeight.value + 10,
+      height: menuHeight.value + menuProps.value.items.length * 4,
       opacity: opacityAnimation(),
       transform: [
         { translateX: translate.beginningTransformations.translateX },
